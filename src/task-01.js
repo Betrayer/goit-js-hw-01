e96767f97f7f7c5d6305bab6bc0fa2171291001f
Betrayer <StarDestroyer@ukr.net>
2020-01-19 20:41:50 +0200

    tryin' to do some hw

---
+++ src/task-01.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let price = 1000;
+
+const name = "Shield generator"
+
+console.log(name, ' selected, price for one item is ',price);
+
+price = 2000; 
+
+console.log(name, ' selected, price for one item is ',price);
